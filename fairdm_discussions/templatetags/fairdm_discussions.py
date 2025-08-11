218912aa1b33ab9e2f06307a2110f810e560a6c7
Sam <samuel.scott.jennings@gmail.com>
2025-08-11 14:23:31 +0200

    rename templatetag

--- fairdm_discussions/templatetags/fairdm_discussions.py
+++ fairdm_discussions/templatetags/fairdm_discussions.py
@@ -1,10 +1,13 @@
 # import flatattrs
 from django import template
+from django.templatetags.static import static
 from django_comments_xtd import VERSION
 
 register = template.Library()
 
 
 @register.simple_tag
-def javascript_version():
-    return "django_comments_xtd/js/django-comments-xtd-{}.{}.{}.js".format(*VERSION)
+def comments_xtd_frontend():
+    return static(
+        "django_comments_xtd/js/django-comments-xtd-{}.{}.{}.js".format(*VERSION)
+    )
